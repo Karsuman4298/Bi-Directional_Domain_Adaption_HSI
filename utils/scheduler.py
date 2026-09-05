ce7a2a647b945c817eaf11ff495d7ccb61a9d858
suman kar <sumankar@sumans-MacBook-Air.local>
2026-09-05 19:11:59 +0530

    Fix NoneType optimizer error for SelfAttnAgentBiDA

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -33,7 +33,7 @@ def load_scheduler(model_name, model, opts):
         # optimizer = optim.Adam(model.parameters(), lr=5e-4)
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [30, 60, 90, 120, 150, 180, 210, 240, 270], gamma=0.9)
 
-    elif model_name in ['ssftt', 'BiDA', 'BiDA_Agent', 'AgentBiDA']:
+    elif model_name in ['ssftt', 'BiDA', 'BiDA_Agent', 'AgentBiDA', 'SelfAttnAgentBiDA']:
         # optimizer = optim.Adam(model.parameters(), lr=opts.lr)
         optimizer = optim.SGD(model.parameters(), lr=opts.lr)
         scheduler = None
